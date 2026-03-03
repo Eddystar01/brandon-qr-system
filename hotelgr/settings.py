f56bf72e0b130dd7ca1edb778af48f1b0fecc06b
Eddy Dev <olanrewajuolamide.o03@gmail.com>
2026-03-03 17:43:25 +0100

    Configured whitenoise for static files

--- hotelgr/settings.py
+++ hotelgr/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'hotelgr.urls'
@@ -124,6 +125,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = []
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import os
 
 MEDIA_URL = '/media/'
