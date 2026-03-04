58b2ac378c300f5fe7351e19c4af2632b30fa81b
Eddy Dev <olanrewajuolamide.o03@gmail.com>
2026-03-04 16:46:39 +0100

    Switch media storage fully to Cloudinary

--- hotelgr/settings.py
+++ hotelgr/settings.py
@@ -141,11 +141,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = []
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-import os
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/kitchen/'
 LOGOUT_REDIRECT_URL = '/login/'
