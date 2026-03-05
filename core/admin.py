01374a70be39a38cad60983f33f23fbb7e5638de
Eddy Dev <olanrewajuolamide.o03@gmail.com>
2026-03-05 12:23:28 +0100

    Brand Django admin

--- core/admin.py
+++ core/admin.py
@@ -31,3 +31,9 @@ class MenuItemAdmin(admin.ModelAdmin):
 
 admin.site.register(MenuItem, MenuItemAdmin)
 admin.site.register(Category)
+
+from django.contrib import admin
+
+admin.site.site_header = "Brandon Hotel & Apartments"
+admin.site.site_title = "Brandon Admin"
+admin.site.index_title = "Brandon Hotel &Management"
