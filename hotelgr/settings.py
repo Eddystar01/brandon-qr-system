44c857d5b9bb0a5e81f4ab3cece703bc477485fc
Eddy Dev <olanrewajuolamide.o03@gmail.com>
2026-03-05 16:37:43 +0100

    Fix production settings: whitenoise order, allowed hosts, secure proxy header

--- hotelgr/settings.py
+++ hotelgr/settings.py
@@ -28,7 +28,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     'qr.brandonhotelandapartments.com',
-    '.onrender.com'
+    '.onrender.com',
+    '127.0.0.1',
+    'localhost'
 ]
 
 # Application definition
@@ -47,13 +49,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'hotelgr.urls'
@@ -167,16 +169,4 @@ STORAGES = {
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-LOGGING = {
-    'version': 1,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'ERROR',
-    },
-}
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
