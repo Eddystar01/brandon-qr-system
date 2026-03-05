1eccb30180bf8df23ef26f3387ed5b70bdafe1ee
Eddy Dev <olanrewajuolamide.o03@gmail.com>
2026-03-05 17:02:26 +0100

    Fix static files for production (whitenoise manifest)

--- hotelgr/settings.py
+++ hotelgr/settings.py
@@ -137,8 +137,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = []
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = '/admin/login/'
