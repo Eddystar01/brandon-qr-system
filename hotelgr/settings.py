bef9effd2e967464976909cf4fd6b8afac21a3fb
Eddy Dev <olanrewajuolamide.o03@gmail.com>
2026-03-05 16:42:45 +0100

    debug fix

--- hotelgr/settings.py
+++ hotelgr/settings.py
@@ -24,7 +24,7 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     'qr.brandonhotelandapartments.com',
